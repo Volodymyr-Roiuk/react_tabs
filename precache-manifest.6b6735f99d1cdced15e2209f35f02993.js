5067ed4873fa1b5417cbc1c588755bacc7b898b5
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-02 22:04:43 +0300

    Updates

---
+++ precache-manifest.6b6735f99d1cdced15e2209f35f02993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c74bad239a8ad0d45e6ce04020be23f",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "cb9d365c58601520b9cc",
+    "url": "/react_tabs/static/css/main.eb574ee0.chunk.css"
+  },
+  {
+    "revision": "7178a2b227e2db7d1ca6",
+    "url": "/react_tabs/static/js/2.3a886142.chunk.js"
+  },
+  {
+    "revision": "cb9d365c58601520b9cc",
+    "url": "/react_tabs/static/js/main.0c9939bc.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
